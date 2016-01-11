b4fc4fe2ed7ea65ebec95b14f0d4ed51dc0666ca
orangepek0e <georgejohnston13@hotmail.com>
2016-01-11 13:12:11 -0400

    I don't know where I'm going from here, but I promise it won't be boring.

--- index.js
+++ index.js
@@ -7,12 +7,41 @@ var user = express.Router();
 user.get('/', function(req, res) { });
 // A POST to the root of a resource should create a new object
 user.post('/', function(req, res) { });
+
+function lookupUser(req, res, next) {
+    // We access the ID param on the request object
+    var username = req.params.username;
+    // We first specify which collection we want to look in
+    var collection = mongo.db.collection('users');
+    // Then we build our lookup, specifying the key name and value
+    var result = collection.find({ username: username });
+    // We convert the result to an array that we can read
+    result.toArray(function(err, users) {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            return res.json({ errors: ['Could not retrieve user']});
+        }
+        // No results returned means the object is not found
+        if (users && users.length === 0) {
+            // We are able to set the HTTP status code on the res object
+            res.statusCode = 404;
+            return res.json({ errors: ['User not found'] });
+        }
+        // By attaching a User property to the request
+        // its data is now made available in our handler function
+        req.user = users[0];
+        // Next tells the app to continue to our handler function
+        next();
+    });
+}
+
 // We specify a param in our path for the GET of a specific object
-user.get('/:username', function(req, res) { });
+user.get('/:username', lookupUser, function(req, res) { });
 // Similar to the GET on an object, to update it we can PATCH
-user.patch('/:username', function(req, res) { });
+user.patch('/:username', lookupUser, function(req, res) { });
 // Delete a specific object
-user.delete('/:username', function(req, res) { });
+user.delete('/:username', lookupUser, function(req, res) { });
 // Now we must attach the router to its desired path
 app.use('/user', user);
 
